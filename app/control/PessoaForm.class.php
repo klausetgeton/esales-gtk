76eedbbaf5e02662aabadf36ca50424acd03240b
Klaus Etgeton <klausetgeton@gmail.com>
2015-11-23 18:57:20 -0200

    Corrigido a cidade no cadastro da pessoa

--- app/control/PessoaForm.class.php
+++ app/control/PessoaForm.class.php
@@ -35,31 +35,50 @@ class PessoaForm extends TStandardForm
         $email                          = new TEntry('email');
         $endereco                       = new TEntry('endereco');
         $numero                         = new TEntry('numero');
+        
         $cidade_id                      = new TSeekButton('cidade_id');
+        $cidade_id->addValidation('Cidade', new TRequiredValidator);
+        $cidade_name                    = new TEntry('cidade_name');
+
+        $obj = new TStandardSeek;
+        $action = new TAction(array($obj, 'onSetup'));
+        $action->setParameter('database',      'esales');
+        $action->setParameter('parent',        'form_Pessoa');
+        $action->setParameter('model',         'Cidade');
+        $action->setParameter('display_field', 'descricao');
+        $action->setParameter('receive_key',   'cidade_id');
+        $action->setParameter('receive_field', 'cidade_name');
+        $cidade_id->setAction($action);
+
+
+
         $cep                            = new TEntry('cep');
         $cpf_cnpj                       = new TEntry('cpf_cnpj');
-        $tipo_pessoa                    = new TEntry('tipo_pessoa');
+        $tipo_pessoa                    = new TRadioGroup('tipo_pessoa');
+        $tipo_pessoa->addItems(array('F' => 'Fisica', 'J' => 'Juridica'));
 
 
         // add the fields
-        $this->form->addQuickField('id', $id,  100);
+        $this->form->addQuickField('Codigo', $id,  100);
         $this->form->addQuickField('Nome', $nome,  200);
         $this->form->addQuickField('Telefone', $telefone,  200);
-        $this->form->addQuickField('email', $email,  200);
-        $this->form->addQuickField('endereço', $endereco,  200);
-        $this->form->addQuickField('numero', $numero,  200);
-        $this->form->addQuickField('Cidade', $cidade_id,  100, new TRequiredValidator );
-        $this->form->addQuickField('cep', $cep,  200);
-        $this->form->addQuickField('cpf_cnpj', $cpf_cnpj,  100);
-        $this->form->addQuickField('tipo_pessoa', $tipo_pessoa,  200);
+        $this->form->addQuickField('Email', $email,  200);
+        $this->form->addQuickField('Endereço', $endereco,  200);
+        $this->form->addQuickField('Numero', $numero,  200);
+
+        $this->form->addQuickFields('Cidade', array($cidade_id, $cidade_name), true);
+
+        $this->form->addQuickField('CEP', $cep,  200);
+        $this->form->addQuickField('CPF/CNPJ', $cpf_cnpj,  100);
+        $this->form->addQuickField('Tipo de pessoa', $tipo_pessoa,  200);
 
 
 
 
         
         // create the form actions
-        $this->form->addQuickAction(_t('Save'), new TAction(array($this, 'onSave')), 'ico_save.png');
-        $this->form->addQuickAction(_t('New'),  new TAction(array($this, 'onEdit')), 'ico_new.png');
+        $this->form->addQuickAction('Salvar', new TAction(array($this, 'onSave')), 'ico_save.png');
+        $this->form->addQuickAction('Novo',  new TAction(array($this, 'onEdit')), 'ico_new.png');
         
         // add the form to the page
         parent::add($this->form);
